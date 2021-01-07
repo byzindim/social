826ec513802e726690a7620511caffe1f9a65a2f
Dim <dmitry.byzin@yandex.ru>
2021-01-07 21:31:00 +0700

    adaptive

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import '../App.css';
+export default function Nav() {
+    return (
+        <nav className="wrapper-nav">
+            <div><a>Profile</a></div>
+            <div><a>Messages</a></div>
+            <div><a>News</a></div>
+            <div><a>Music</a></div>
+            <div><a>Settings</a></div>
+        </nav>
+    )
+}
